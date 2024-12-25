e4d0e1591325f0bbc89ddc58966ed0c586a592d3
Sujina21 <sujinashrestha060@gmail.com>
2024-12-25 20:26:16 +0545

    login succesfull

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,7 @@ const auth = require('../middlewares/auth');
 const userController = require('../controllers/userController');
 
 router.post('/register', userController.register_new_user);
+router.post('/login', userController.login_user);
 
 
 module.exports = router;
