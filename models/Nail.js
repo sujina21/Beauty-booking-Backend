6db344860c14e8ec70c84f1813a2cf52d904021a
Sujina21 <sujinashrestha060@gmail.com>
2024-12-26 12:30:56 +0545

    Nail Model Done

---
+++ models/Nail.js
@@ -0,0 +1,35 @@
+const { hash } = require('bcryptjs');
+const mongoose = require('mongoose');
+
+const nailSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+    },
+    createdDate: {  
+        type: Date,
+        default: Date.now
+    },
+    hashtag:[String],
+    avgRating: {
+        type: Number,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model('Nail', nailSchema);
