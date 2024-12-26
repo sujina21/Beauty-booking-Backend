f0404bf598b0739cd316d04e882d2984e9006e4d
Sujina21 <sujinashrestha060@gmail.com>
2024-12-26 13:02:57 +0545

    nail service with image added

--- controllers/nailController.js
+++ controllers/nailController.js
@@ -38,3 +38,38 @@ module.exports.add_nail_service = async (req, res) => {
 }
 
 
+exports.update_nail_service = async (req, res) => {
+    try{
+        const nailId = req.params.id;
+        const nail = await Nail.findById(nailId);
+        if(nail){
+            if(req.files !==undefined){
+                const formImage = req.files.image;
+                const imagePath = formImage.tempFilePath;
+                if(formImage.mimetype == "image/jpeg" || formImage.mimetype == "image/jpg" || formImage.mimetype == "image/png") {
+                    const image = await cloudinary.upload_image(imagePath);
+                    nail.title = req.body.title;
+                    nail.price = req.body.price;
+                    nail.duration = req.body.duration;
+                    nail.image = image;
+                    const result = await nail.save();
+                    res.json(success("Nail Service with Image Updated Successfully", result));
+                } else {
+                    res.json(failure("Must be png, jpg or jpeg"));
+                }
+            } else {
+                nail.title = req.body.title;
+                nail.price = req.body.price;
+                nail.duration = req.body.duration;
+                const result = await nail.save();
+                res.json(success("Nail Service Updated Successfully", result));
+            }
+        } else {
+            res.json(failure("Nail Service Not Found"));
+        }
+    } catch(err){
+        console.log(err);
+        res.json(failure("Something went wrong"));
+    }
+    res.end();
+}
