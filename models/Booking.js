1c6a8c7482b54a83a0af82aa3d5b4fdf5853fe8b
Sujina21 <sujinashrestha060@gmail.com>
2025-03-02 16:30:45 +0545

    S02-T06: Added Booking model schema

---
+++ models/Booking.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const BookingSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  serviceId: { type: mongoose.Schema.Types.ObjectId, refPath: "serviceType", required: true },
+  serviceType: { type: String, enum: ["Makeup", "Nail"], required: true },
+  bookingDate: { type: String, required: true },
+  bookingTime: { type: String, required: true },
+  status: { type: String, enum: ["Pending", "Confirmed", "Cancelled"], default: "Pending" },
+});
+
+module.exports = mongoose.model("Booking", BookingSchema);
