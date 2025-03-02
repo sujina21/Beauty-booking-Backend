804227a7de403a537de931a91b34703ca5a9ba50
Sujina21 <sujinashrestha060@gmail.com>
2025-03-02 16:30:32 +0545

    S02-T04: Updated user routes with additional checks

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -11,4 +11,7 @@ router.patch('/', auth.verifyUser, userController.update_user_profile);
 router.post('/reset-code', auth.verifyUser, userController.reset_password);
 router.post('/reset-password', auth.verifyUser, userController.create_new_password);
 
+// Route to get user details (for admin)
+router.get('/all', auth.verifyUser, userController.get_all_users);
+router.patch('/role/:userId/:status/', auth.verifyUser, userController.update_user_role);    
 module.exports = router;
